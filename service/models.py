1febbe95004726f6abedcce1fbd1de78b48adbe7
VR <vladrymarchuk@gmail.com>
2025-02-14 23:41:41 +0200

    feat: add str for author

--- service/models.py
+++ service/models.py
@@ -7,6 +7,9 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Book(models.Model):
     class CoverType(models.TextChoices):
