255f02a54b6771860071e9a3d29d645f3f7a537f
VR <vladrymarchuk@gmail.com>
2025-02-14 23:10:12 +0200

    feat: add admin for author

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from service.models import Book, Borrowing
+from service.models import Book, Borrowing, Author
 
+admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Borrowing)
