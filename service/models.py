c8194781ba202e8e37d803c17cf7cdad466b43d5
VR <vladrymarchuk@gmail.com>
2025-02-14 23:07:39 +0200

    update: add author model and change in book

--- service/models.py
+++ service/models.py
@@ -3,14 +3,20 @@ from django.db import models
 from config.settings.base import AUTH_USER_MODEL
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
 class Book(models.Model):
     class CoverType(models.TextChoices):
         hard = "HARD"
         soft = "SOFT"
 
     title = models.CharField(max_length=100)
-    author = models.ManyToManyField(
-        AUTH_USER_MODEL,
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
         related_name="books",
     )
     cover = models.CharField(
