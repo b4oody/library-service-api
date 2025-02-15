32076a8c81797cfcbd97ea25ad009ccee98332c9
VR <vladrymarchuk@gmail.com>
2025-02-15 16:52:11 +0200

    feat: create urls for service, user

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/v1/library-service/",
+        include("service.urls"),
+        name="library-service"
+    ),
+    path(
+        "api/v1/library-service/user/",
+        include("user.urls"),
+        name="user"
+    ),
 ]
