813f4f0a589f91dc3afbb6ebe81968b2b9912181
VR <vladrymarchuk@gmail.com>
2025-02-16 00:42:23 +0200

    feat: add permission for author and book

--- service/views.py
+++ service/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from service import permissions
 from service.models import Author, Book, Borrowing
 from service.serializers import (
     AuthorSerializer,
@@ -11,11 +12,13 @@ from service.serializers import (
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAdminOrReadOnly]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminOrReadOnly]
 
 
 class BorrowingSerializerViewSet(viewsets.ModelViewSet):
