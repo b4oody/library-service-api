e216ca546a2d22a3fee68df2be63daef145c6a76
VR <vladrymarchuk@gmail.com>
2025-02-16 00:41:57 +0200

    feat: add authentication for testing by token

--- config/settings/base.py
+++ config/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "service",
     "user",
     "rest_framework",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
