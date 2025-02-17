928cbde40508b6d50a7aae2522bfce70c46e77ef
VR <vladrymarchuk@gmail.com>
2025-02-17 16:56:59 +0200

    feat: add models for telegram_bot

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from config.settings.base import AUTH_USER_MODEL
+
+
+class TelegramUser(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    chat_id = models.BigIntegerField(unique=True)
+
+    def __str__(self):
+        return self.user
