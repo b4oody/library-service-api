fff8bd171b76033d376fbc5ac159132a71ac9700
VR <vladrymarchuk@gmail.com>
2025-02-18 23:25:56 +0200

    update: add unique_token for TelegramUser model and migrate

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 from config.settings.base import AUTH_USER_MODEL
@@ -5,7 +7,8 @@ from config.settings.base import AUTH_USER_MODEL
 
 class TelegramUser(models.Model):
     user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    chat_id = models.BigIntegerField(unique=True)
+    telegram_id = models.CharField(max_length=255, null=True, blank=True)
+    unique_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.user
