ccbc2430b3102cff950954487e61829dbe96b0ab
VR <vladrymarchuk@gmail.com>
2025-02-19 15:17:41 +0200

    feat: add notifications for create, return borrowing in telegram_bot

--- service/views.py
+++ service/views.py
@@ -13,6 +13,8 @@ from service.serializers import (
     BorrowingCreateSerializer,
     ReturnBookSerializer,
 )
+from telegram_bot.models import TelegramUser
+from telegram_bot.utilities import send_telegram_message
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -54,7 +56,25 @@ class BorrowingSerializerViewSet(viewsets.ModelViewSet):
         return BorrowingCreateSerializer
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        borrowing = serializer.save(user=self.request.user)
+
+        try:
+            telegram_user = TelegramUser.objects.get(user=self.request.user)
+            telegram_id = telegram_user.telegram_id
+
+            message = (
+                f"*Вітаємо {telegram_user.user}!* \n\n"
+                f"Ваше запозичення №{borrowing.id} було успішно створено.\n\n"
+                f"*Запозичена книга*: {borrowing.book}\n"
+                f"*Дата позичання*: {borrowing.borrow}\n"
+                f"*Повернути до*: {borrowing.expected_return}\n\n"
+                f"Бажаємо приємного читання!"
+            )
+
+            send_telegram_message(telegram_id, message)
+
+        except TelegramUser.DoesNotExist:
+            print(f"Не знайдено Telegram ID для користувача {self.request.user.username}")
 
 
 class ReturnBookBorrowing(
@@ -72,6 +92,17 @@ class ReturnBookBorrowing(
         borrowing.book.inventory += 1
         borrowing.book.save()
         borrowing.save()
+        telegram_user = TelegramUser.objects.get(user=self.request.user)
+        telegram_id = telegram_user.telegram_id
+        message = (
+            f"*Вітаємо {telegram_user.user}!* \n\n"
+            f"Ваше запозичення №{borrowing.id} було успішно повернуто.\n\n"
+            f"*Запозичена книга*: {borrowing.book}\n"
+            f"*Дата позичання*: {borrowing.borrow}\n"
+            f"*Повернуто*: {borrowing.actual}\n\n"
+            f"Дякую за співпрацю!"
+        )
+        send_telegram_message(telegram_id, message)
         return HttpResponse(
             "The book have returned success!",
             status.HTTP_200_OK
